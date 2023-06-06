ec425d13ece9b2597edc0384e39325fe951a070a
Dylan Wilson <dylwil3@gmail.com>
2023-06-06 10:44:10 -0400

    finish test refactoring

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,86 +1,102 @@
-# def test_default_dict_to_list():
-#     dd = defaultdict(lambda: float("inf"))
-#     dd[0] = 1
-#     dd[1] = 2
-#     dd[2] = 3
-#     e = rs.default_dict_to_list(dd, 2)
-#     assert e[0] == 1
-#     assert e[1] == 2
-#     assert e[2] == 3
-#     assert e[3] == float("inf")
-#     assert len(e) == 4
+from collections import defaultdict
+from typing import DefaultDict
+
+import numpy as np
+import pandas as pd
+
+from reward_shaping.plotting import (
+    default_dict_to_list,
+    filter_inf_start_value,
+    plot_evals,
+    update_dict_to_array,
+    visualize_frozen_updates,
+)
 
 
-# def test_update_dict_to_array():
-#     dd = defaultdict(lambda: float("inf"))
-#     dd[0] = 1
-#     dd[1] = 2
-#     dd[2] = 3
-#     ff = defaultdict(lambda: float("inf"))
-#     ff[0] = 10
-#     ff[1] = 9
-#     v = [dd, ff]
-#     flat = rs.update_dict_to_array(v, 2)
-#     assert len(flat[0]) == 4
-#     assert len(flat[1]) == 4
-#     assert len(flat) == 2
-#     assert flat[0][3] == float("inf")
-#     assert flat[1][2] == float("inf")
-#     assert flat[1][0] == 10
-#     assert flat[0][1] == 2
-#     assert isinstance(flat, np.ndarray)
-#     assert isinstance(flat[0], np.ndarray)
+def test_default_dict_to_list():
+    dd: DefaultDict[int, float] = defaultdict(lambda: float("inf"))
+    dd[0] = 1
+    dd[1] = 2
+    dd[2] = 3
+    e = default_dict_to_list(dd, 2)
+    assert e[0] == 1
+    assert e[1] == 2
+    assert e[2] == 3
+    assert e[3] == float("inf")
+    assert len(e) == 4
 
 
-# def test_visualize_frozen_updates():
-#     dd = defaultdict(lambda: float("inf"))
-#     dd[0] = 1
-#     dd[1] = 2
-#     dd[2] = 3
-#     ff = defaultdict(lambda: float("inf"))
-#     ff[1] = 9
-#     ff[2] = 8
-#     ax, len1, len2 = rs.visualize_frozen_updates([dd, ff], 2)
-#     assert ax.get_title() == "Timesteps before Updating Value"
-#     assert len1 == 1
-#     assert len2 == 2
+def test_update_dict_to_array():
+    dd = defaultdict(lambda: float("inf"))
+    dd[0] = 1
+    dd[1] = 2
+    dd[2] = 3
+    ff = defaultdict(lambda: float("inf"))
+    ff[0] = 10
+    ff[1] = 9
+    v = [dd, ff]
+    flat = update_dict_to_array(v, 2)
+    assert len(flat[0]) == 4
+    assert len(flat[1]) == 4
+    assert len(flat) == 2
+    assert flat[0][3] == float("inf")
+    assert flat[1][2] == float("inf")
+    assert flat[1][0] == 10
+    assert flat[0][1] == 2
+    assert isinstance(flat, np.ndarray)
+    assert isinstance(flat[0], np.ndarray)
 
 
-# def test_filter_inf_start_value():
-#     arr = [
-#         [float("inf"), 8, 9],
-#         [1, 2, 3],
-#         [float("inf"), 5, 7],
-#         [0, 0, 0],
-#     ]
-#     arr = np.array(arr)
-#     finite, infinite = rs.filter_inf_start_value(arr)
-#     assert (
-#         infinite
-#         == np.array(
-#             [
-#                 [float("inf"), 8, 9],
-#                 [float("inf"), 5, 7],
-#             ]
-#         )
-#     ).all()
-#     assert (
-#         finite
-#         == np.array(
-#             [
-#                 [1, 2, 3],
-#                 [0, 0, 0],
-#             ]
-#         )
-#     ).all()
+def test_visualize_frozen_updates():
+    dd = defaultdict(lambda: float("inf"))
+    dd[0] = 1
+    dd[1] = 2
+    dd[2] = 3
+    ff = defaultdict(lambda: float("inf"))
+    ff[1] = 9
+    ff[2] = 8
+    ax, len1, len2 = visualize_frozen_updates([dd, ff], 2)
+    assert ax.get_title() == "Timesteps before Updating Value"
+    assert len1 == 1
+    assert len2 == 2
 
-# def test_plot_evals():
-#     df = pd.DataFrame(
-#         [
-#             [1, 2, 3],
-#             [0, 0, 0],
-#             [1, 2, 3],
-#         ]
-#     )
-#     ax = rs.plot_evals(df)
-#     assert ax
+
+def test_filter_inf_start_value():
+    arr = [
+        [float("inf"), 8, 9],
+        [1, 2, 3],
+        [float("inf"), 5, 7],
+        [0, 0, 0],
+    ]
+    arr = np.array(arr)
+    finite, infinite = filter_inf_start_value(arr)
+    assert (
+        infinite
+        == np.array(
+            [
+                [float("inf"), 8.0, 9.0],
+                [float("inf"), 5.0, 7.0],
+            ]
+        )
+    ).all()
+    assert (
+        finite
+        == np.array(
+            [
+                [1, 2, 3],
+                [0, 0, 0],
+            ]
+        )
+    ).all()
+
+
+def test_plot_evals():
+    df = pd.DataFrame(
+        [
+            [1, 2, 3],
+            [0, 0, 0],
+            [1, 2, 3],
+        ]
+    )
+    ax = plot_evals(df)
+    assert ax is not None
--- tests/test_wrappers.py
+++ tests/test_wrappers.py
@@ -1,8 +1,37 @@
-# def test_bad_wrapper_init():
-#     env = gym.make("FrozenLake-v1")
-#     wrapped = rs.RewardGettingCloser(env, lambda x: 0)
-#     assert wrapped
+import gymnasium as gym
 
-# def test_PBRS_wrapper():
-#     wrapped = rs.PBRS(gym.make("FrozenLake-v1"), potential=lambda x: 10, discount=0.95)
-#     assert wrapped
+import reward_shaping as rs
+
+
+def test_bad_wrapper_init():
+    env = gym.make("FrozenLake-v1")
+    wrapped = rs.RewardGettingCloser(env, lambda x: 0)
+    assert wrapped
+
+
+def test_PBRS_wrapper_init():
+    wrapped = rs.PBRS(gym.make("FrozenLake-v1"), potential=lambda x: 10, discount=0.95)
+    assert wrapped
+
+
+def test_PBRS_wrapper_expected_reward():
+    wrapped = rs.PBRS(
+        gym.make("FrozenLake-v1", is_slippery=False),
+        potential=lambda x: 10,
+        discount=0.95,
+    )
+    wrapped.reset()
+    next_obs, rew, term, trunc, _ = wrapped.step(1)
+    assert next_obs == 4
+    assert not term
+    assert not trunc
+    assert rew == (10 * 0.95 - 10)
+    assert wrapped.curr_obs == 4
+    wrapped.reset()
+    assert wrapped.curr_obs == 0
+    next_obs, rew, term, trunc, _ = wrapped.step(1)
+    assert next_obs == 4
+    assert not term
+    assert not trunc
+    assert rew == (10 * 0.95 - 10)
+    assert wrapped.curr_obs == 4
